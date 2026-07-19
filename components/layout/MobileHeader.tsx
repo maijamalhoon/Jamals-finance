6a425dc1234699baf70f8197fc82020503f58913
Jamal Yaqoob <150429791+maijamalhoon@users.noreply.github.com>
2026-07-19 11:23:01 +0500

    feat: enable mobile sidebar swipe navigation

--- components/layout/MobileHeader.tsx
+++ components/layout/MobileHeader.tsx
@@ -3,6 +3,7 @@
 import type { ReactNode } from "react";
 
 import MobileNav from "@/components/layout/MobileNav";
+import MobileNavSwipeGestures from "@/components/layout/MobileNavSwipeGestures";
 
 type MobileHeaderProps = {
   notificationSlot: ReactNode;
@@ -11,5 +12,10 @@ type MobileHeaderProps = {
 export default function MobileHeader({
   notificationSlot,
 }: MobileHeaderProps) {
-  return <MobileNav notificationSlot={notificationSlot} />;
+  return (
+    <>
+      <MobileNav notificationSlot={notificationSlot} />
+      <MobileNavSwipeGestures />
+    </>
+  );
 }
