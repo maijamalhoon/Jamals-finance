5fdde3caef632fb0b66bef2156710e0589d4ea23
Jamal Yaqoob <150429791+maijamalhoon@users.noreply.github.com>
2026-07-24 00:23:55 +0500

    feat(icons): add JALVORO navigation components

---
+++ components/icons/jalvoro/components/navigation.tsx
@@ -0,0 +1,28 @@
+import { createJalvoroIcon } from "../core";
+import {
+  dashboardIconDefinition,
+  transactionsIconDefinition,
+  accountsIconDefinition,
+  incomeIconDefinition,
+  expensesIconDefinition,
+  goalsIconDefinition,
+  payablesIconDefinition,
+  investmentsIconDefinition,
+  analyticsIconDefinition,
+  aiInsightsIconDefinition,
+  reportsIconDefinition,
+  settingsIconDefinition,
+} from "../definitions/navigation";
+
+export const JalvoroDashboardIcon = /* @__PURE__ */ createJalvoroIcon(dashboardIconDefinition);
+export const JalvoroTransactionsIcon = /* @__PURE__ */ createJalvoroIcon(transactionsIconDefinition);
+export const JalvoroAccountsIcon = /* @__PURE__ */ createJalvoroIcon(accountsIconDefinition);
+export const JalvoroIncomeIcon = /* @__PURE__ */ createJalvoroIcon(incomeIconDefinition);
+export const JalvoroExpensesIcon = /* @__PURE__ */ createJalvoroIcon(expensesIconDefinition);
+export const JalvoroGoalsIcon = /* @__PURE__ */ createJalvoroIcon(goalsIconDefinition);
+export const JalvoroPayablesIcon = /* @__PURE__ */ createJalvoroIcon(payablesIconDefinition);
+export const JalvoroInvestmentsIcon = /* @__PURE__ */ createJalvoroIcon(investmentsIconDefinition);
+export const JalvoroAnalyticsIcon = /* @__PURE__ */ createJalvoroIcon(analyticsIconDefinition);
+export const JalvoroAiInsightsIcon = /* @__PURE__ */ createJalvoroIcon(aiInsightsIconDefinition);
+export const JalvoroReportsIcon = /* @__PURE__ */ createJalvoroIcon(reportsIconDefinition);
+export const JalvoroSettingsIcon = /* @__PURE__ */ createJalvoroIcon(settingsIconDefinition);
