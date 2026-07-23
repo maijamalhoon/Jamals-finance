185972109c5b825cb74cece4349dc0b5a306e48c
Jamal Yaqoob <150429791+maijamalhoon@users.noreply.github.com>
2026-07-24 00:26:27 +0500

    feat(icons): add JALVORO finance components

---
+++ components/icons/jalvoro/components/finance.tsx
@@ -0,0 +1,36 @@
+import { createJalvoroIcon } from "../core";
+import {
+  walletIconDefinition,
+  bankIconDefinition,
+  cardIconDefinition,
+  cashIconDefinition,
+  coinIconDefinition,
+  receiptIconDefinition,
+  invoiceIconDefinition,
+  budgetIconDefinition,
+  savingsIconDefinition,
+  trendUpIconDefinition,
+  trendDownIconDefinition,
+  transferIconDefinition,
+  exchangeIconDefinition,
+  calendarMoneyIconDefinition,
+  shieldMoneyIconDefinition,
+  taxIconDefinition,
+} from "../definitions/finance";
+
+export const JalvoroWalletIcon = /* @__PURE__ */ createJalvoroIcon(walletIconDefinition);
+export const JalvoroBankIcon = /* @__PURE__ */ createJalvoroIcon(bankIconDefinition);
+export const JalvoroCardIcon = /* @__PURE__ */ createJalvoroIcon(cardIconDefinition);
+export const JalvoroCashIcon = /* @__PURE__ */ createJalvoroIcon(cashIconDefinition);
+export const JalvoroCoinIcon = /* @__PURE__ */ createJalvoroIcon(coinIconDefinition);
+export const JalvoroReceiptIcon = /* @__PURE__ */ createJalvoroIcon(receiptIconDefinition);
+export const JalvoroInvoiceIcon = /* @__PURE__ */ createJalvoroIcon(invoiceIconDefinition);
+export const JalvoroBudgetIcon = /* @__PURE__ */ createJalvoroIcon(budgetIconDefinition);
+export const JalvoroSavingsIcon = /* @__PURE__ */ createJalvoroIcon(savingsIconDefinition);
+export const JalvoroTrendUpIcon = /* @__PURE__ */ createJalvoroIcon(trendUpIconDefinition);
+export const JalvoroTrendDownIcon = /* @__PURE__ */ createJalvoroIcon(trendDownIconDefinition);
+export const JalvoroTransferIcon = /* @__PURE__ */ createJalvoroIcon(transferIconDefinition);
+export const JalvoroExchangeIcon = /* @__PURE__ */ createJalvoroIcon(exchangeIconDefinition);
+export const JalvoroCalendarMoneyIcon = /* @__PURE__ */ createJalvoroIcon(calendarMoneyIconDefinition);
+export const JalvoroShieldMoneyIcon = /* @__PURE__ */ createJalvoroIcon(shieldMoneyIconDefinition);
+export const JalvoroTaxIcon = /* @__PURE__ */ createJalvoroIcon(taxIconDefinition);
