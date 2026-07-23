368e6d4df632aff0d74f22513bbc7deb1086aa1c
Jamal Yaqoob <150429791+maijamalhoon@users.noreply.github.com>
2026-07-24 00:26:14 +0500

    feat(icons): add JALVORO action components

---
+++ components/icons/jalvoro/components/actions.tsx
@@ -0,0 +1,40 @@
+import { createJalvoroIcon } from "../core";
+import {
+  addIconDefinition,
+  editIconDefinition,
+  deleteIconDefinition,
+  copyIconDefinition,
+  searchIconDefinition,
+  filterIconDefinition,
+  sortIconDefinition,
+  shareIconDefinition,
+  exportIconDefinition,
+  importIconDefinition,
+  downloadIconDefinition,
+  uploadIconDefinition,
+  refreshIconDefinition,
+  checkIconDefinition,
+  closeIconDefinition,
+  moreIconDefinition,
+  undoIconDefinition,
+  redoIconDefinition,
+} from "../definitions/actions";
+
+export const JalvoroAddIcon = /* @__PURE__ */ createJalvoroIcon(addIconDefinition);
+export const JalvoroEditIcon = /* @__PURE__ */ createJalvoroIcon(editIconDefinition);
+export const JalvoroDeleteIcon = /* @__PURE__ */ createJalvoroIcon(deleteIconDefinition);
+export const JalvoroCopyIcon = /* @__PURE__ */ createJalvoroIcon(copyIconDefinition);
+export const JalvoroSearchIcon = /* @__PURE__ */ createJalvoroIcon(searchIconDefinition);
+export const JalvoroFilterIcon = /* @__PURE__ */ createJalvoroIcon(filterIconDefinition);
+export const JalvoroSortIcon = /* @__PURE__ */ createJalvoroIcon(sortIconDefinition);
+export const JalvoroShareIcon = /* @__PURE__ */ createJalvoroIcon(shareIconDefinition);
+export const JalvoroExportIcon = /* @__PURE__ */ createJalvoroIcon(exportIconDefinition);
+export const JalvoroImportIcon = /* @__PURE__ */ createJalvoroIcon(importIconDefinition);
+export const JalvoroDownloadIcon = /* @__PURE__ */ createJalvoroIcon(downloadIconDefinition);
+export const JalvoroUploadIcon = /* @__PURE__ */ createJalvoroIcon(uploadIconDefinition);
+export const JalvoroRefreshIcon = /* @__PURE__ */ createJalvoroIcon(refreshIconDefinition);
+export const JalvoroCheckIcon = /* @__PURE__ */ createJalvoroIcon(checkIconDefinition);
+export const JalvoroCloseIcon = /* @__PURE__ */ createJalvoroIcon(closeIconDefinition);
+export const JalvoroMoreIcon = /* @__PURE__ */ createJalvoroIcon(moreIconDefinition);
+export const JalvoroUndoIcon = /* @__PURE__ */ createJalvoroIcon(undoIconDefinition);
+export const JalvoroRedoIcon = /* @__PURE__ */ createJalvoroIcon(redoIconDefinition);
