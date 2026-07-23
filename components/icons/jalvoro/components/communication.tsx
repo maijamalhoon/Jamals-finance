0285b3ede5834e0dcb3de47dc6b1da60a8920bb8
Jamal Yaqoob <150429791+maijamalhoon@users.noreply.github.com>
2026-07-24 00:26:57 +0500

    feat(icons): add JALVORO communication components

---
+++ components/icons/jalvoro/components/communication.tsx
@@ -0,0 +1,14 @@
+import { createJalvoroIcon } from "../core";
+import {
+  mailIconDefinition,
+  chatIconDefinition,
+  phoneIconDefinition,
+  sendIconDefinition,
+  globeIconDefinition,
+} from "../definitions/communication";
+
+export const JalvoroMailIcon = /* @__PURE__ */ createJalvoroIcon(mailIconDefinition);
+export const JalvoroChatIcon = /* @__PURE__ */ createJalvoroIcon(chatIconDefinition);
+export const JalvoroPhoneIcon = /* @__PURE__ */ createJalvoroIcon(phoneIconDefinition);
+export const JalvoroSendIcon = /* @__PURE__ */ createJalvoroIcon(sendIconDefinition);
+export const JalvoroGlobeIcon = /* @__PURE__ */ createJalvoroIcon(globeIconDefinition);
