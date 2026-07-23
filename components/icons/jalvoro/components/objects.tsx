23db3f29b48087fd2dcd996a8e3509a4b37800e8
Jamal Yaqoob <150429791+maijamalhoon@users.noreply.github.com>
2026-07-24 00:26:39 +0500

    feat(icons): add JALVORO object components

---
+++ components/icons/jalvoro/components/objects.tsx
@@ -0,0 +1,28 @@
+import { createJalvoroIcon } from "../core";
+import {
+  fileIconDefinition,
+  folderIconDefinition,
+  bellIconDefinition,
+  clockIconDefinition,
+  calendarIconDefinition,
+  tagIconDefinition,
+  linkIconDefinition,
+  imageIconDefinition,
+  cameraIconDefinition,
+  lockIconDefinition,
+  keyIconDefinition,
+  pencilIconDefinition,
+} from "../definitions/objects";
+
+export const JalvoroFileIcon = /* @__PURE__ */ createJalvoroIcon(fileIconDefinition);
+export const JalvoroFolderIcon = /* @__PURE__ */ createJalvoroIcon(folderIconDefinition);
+export const JalvoroBellIcon = /* @__PURE__ */ createJalvoroIcon(bellIconDefinition);
+export const JalvoroClockIcon = /* @__PURE__ */ createJalvoroIcon(clockIconDefinition);
+export const JalvoroCalendarIcon = /* @__PURE__ */ createJalvoroIcon(calendarIconDefinition);
+export const JalvoroTagIcon = /* @__PURE__ */ createJalvoroIcon(tagIconDefinition);
+export const JalvoroLinkIcon = /* @__PURE__ */ createJalvoroIcon(linkIconDefinition);
+export const JalvoroImageIcon = /* @__PURE__ */ createJalvoroIcon(imageIconDefinition);
+export const JalvoroCameraIcon = /* @__PURE__ */ createJalvoroIcon(cameraIconDefinition);
+export const JalvoroLockIcon = /* @__PURE__ */ createJalvoroIcon(lockIconDefinition);
+export const JalvoroKeyIcon = /* @__PURE__ */ createJalvoroIcon(keyIconDefinition);
+export const JalvoroPencilIcon = /* @__PURE__ */ createJalvoroIcon(pencilIconDefinition);
