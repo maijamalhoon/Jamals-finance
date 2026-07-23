9f244316bf880ade56fb853298456240c4c71cb6
Jamal Yaqoob <150429791+maijamalhoon@users.noreply.github.com>
2026-07-24 00:27:10 +0500

    feat(icons): add JALVORO interface components

---
+++ components/icons/jalvoro/components/interface.tsx
@@ -0,0 +1,24 @@
+import { createJalvoroIcon } from "../core";
+import {
+  menuIconDefinition,
+  gridIconDefinition,
+  listIconDefinition,
+  sidebarIconDefinition,
+  eyeIconDefinition,
+  eyeOffIconDefinition,
+  chevronDownIconDefinition,
+  chevronRightIconDefinition,
+  arrowLeftIconDefinition,
+  arrowRightIconDefinition,
+} from "../definitions/interface";
+
+export const JalvoroMenuIcon = /* @__PURE__ */ createJalvoroIcon(menuIconDefinition);
+export const JalvoroGridIcon = /* @__PURE__ */ createJalvoroIcon(gridIconDefinition);
+export const JalvoroListIcon = /* @__PURE__ */ createJalvoroIcon(listIconDefinition);
+export const JalvoroSidebarIcon = /* @__PURE__ */ createJalvoroIcon(sidebarIconDefinition);
+export const JalvoroEyeIcon = /* @__PURE__ */ createJalvoroIcon(eyeIconDefinition);
+export const JalvoroEyeOffIcon = /* @__PURE__ */ createJalvoroIcon(eyeOffIconDefinition);
+export const JalvoroChevronDownIcon = /* @__PURE__ */ createJalvoroIcon(chevronDownIconDefinition);
+export const JalvoroChevronRightIcon = /* @__PURE__ */ createJalvoroIcon(chevronRightIconDefinition);
+export const JalvoroArrowLeftIcon = /* @__PURE__ */ createJalvoroIcon(arrowLeftIconDefinition);
+export const JalvoroArrowRightIcon = /* @__PURE__ */ createJalvoroIcon(arrowRightIconDefinition);
